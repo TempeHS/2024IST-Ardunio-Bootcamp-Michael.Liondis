d3439946c239a7e8ec98c8a86a9e5535685f7c82
MLiondis <Michael.liondis@education.nsw.gov.au>
2023-08-04 11:37:13 +1000

    complete activity 1

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,12 +1,11 @@
 /*
-  Author: 
+  Author: Michael Liondis 2/08/23
   Learning Intention: The students will understand the difference between the inbuilt setup and loop methods, and understand the basics of the serial monitor'.
   Success Criteria:
     1. I understand the different behaviours of the setup and loop methods
     2. I can print information to the serial monitor
     3. I can print information to a new line of the serial monitor
-    4. I can access the serial plotter to see data output over time
-    5. I understand the impact of having more than one setup or loop methods in my sketch
+    4. I understand the impact of having more than one setup or loop methods in my sketch
   
   Student Notes: 
 
@@ -17,10 +16,16 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial monitor is configured to 9600");
+  Serial.println("------------------------------------");
+
 
 }
 
 // The loop function runs over and over again forever
-void loop() {
-
+void loop() { 
+  while(!Serial.available());
+  String myString = Serial.readString();
+  Serial.println(myString);
 }
