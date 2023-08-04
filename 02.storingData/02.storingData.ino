7abf677c95714fa0d8c8c698633e9d31c0c798fc
MLiondis <Michael.liondis@education.nsw.gov.au>
2023-08-04 11:55:41 +1000

    start arduino 02

--- 02.storingData/02.storingData.ino
+++ 02.storingData/02.storingData.ino
@@ -16,8 +16,17 @@
     https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
 */
 
-void setup() {
+String myString = "Hello world";
+int myint = 3;
+bool mybool = false;
+float myfloat = 3.14;
+long mylong = 3.1415926545;
 
+
+void setup() {
+Serial.begin(9600);
+  Serial.println("Serial monitor is configured to 9600");
+  Serial.println("------------------------------------");
 }
 
 void loop() {
